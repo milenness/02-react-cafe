69c6fdf23f4406ad28239a849ea6cdd160f426a0
Milena Karpenko <milenness06@gmail.com>
2026-03-19 11:57:01 +0200

    upd hw

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+import "modern-normalize";
+
+import App from '../src/components/App/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
